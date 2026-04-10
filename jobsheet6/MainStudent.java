8200572a5bca30facff6cb3cf8918ee572d02ec8
Nafi Hanif <naafiik1@gmail.com>
2026-04-10 13:50:16 +0700

    jonsheet 6 week 7 done

---
+++ jobsheet6/MainStudent.java
@@ -0,0 +1,46 @@
+package jobsheet6;
+import java.util.Scanner;
+public class MainStudent {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+
+        SearchStudent data = new SearchStudent();
+        int amountStudent = 5 ;
+
+        System.out.println("-----------------------------");
+        System.out.println("input student accordingly from smallet NIM");
+        for (int i = 0; i < amountStudent; i++){
+        System.out.println("-------------");
+        System.out.print("NIM\t: ");
+        int nim = s.nextInt();
+        System.out.print("Name\t: ");
+        String name = s1.nextLine();
+        System.out.print("Age\t: ");
+        int age = s.nextInt();
+        System.out.print("GPA\t: ");
+        double gpa = s.nextDouble();
+
+        Students std = new Students(nim, age, name, gpa);
+        data.add(std);
+    }
+
+        System.out.println("----------------------------");
+        System.out.println("Entire Student Data ");
+        data.display();
+
+        System.out.println("____________________________");
+        System.out.println("____________________________");
+        System.out.print("search student by NIM: ");
+        int search = s.nextInt();
+        System.out.println("Using Seuential Search");
+        int position = data.findSeqSearch(search);
+
+        data.showPosition(search, position);
+
+        data.showData(search, position);
+        
+    s.close();
+    s1.close();
+    }
+}
